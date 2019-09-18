0ec4947376547ff11a3263dc60b3f67f6ce7f685
Jared S <jaredjames2020@gmail.com>
2019-09-18 23:09:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,3 @@
-# def roll_call_dwarves# code an argument here
-#   # Your code here
-# end
-
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
-
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
-
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
-
 require 'pry'
 def roll_call_dwarves(dwarves)
     dwarves.each.with_index do |name, number|
@@ -38,6 +21,4 @@ def find_the_cheese(cheese)
   # the array below is here to help
     cheese_types = ["cheddar", "gouda", "camembert"]
     cheese.detect {|type| cheese_types.include?(type)}
-  
-  
 end
